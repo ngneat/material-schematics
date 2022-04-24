b1d6317d5e388309a44711835873445f8510c180
Dharmen Shah <shhdharmen@gmail.com>
2022-04-24 21:03:01 +0530

    build: remove types.d.ts from gitignore

---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+type MaterialPalette = "Primary" | "Accent" | "Warn";
+type MaterialTheme = | "indigo-pink"
+| "deeppurple-amber"
+| "pink-bluegray"
+| "purple-green"
+| "custom";
+type MaterialDarkLight = "dark" | "light";
